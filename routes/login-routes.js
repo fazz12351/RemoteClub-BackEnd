5a8b1317bbf990353b9edfe5b0bbe5b925f40c2e
Mohammed Ahmed <mahme029@gold.ac.uk>
2024-01-15 16:55:03 +0000

    Update:Implemented bycrypt to haspassword but causing errors

--- routes/login-routes.js
+++ routes/login-routes.js
@@ -3,12 +3,34 @@ const app = express();
 
 const mongodb = require("mongodb")
 const mongoose = require("mongoose")
+const { registerTradesman, hashPassword, comparePasswords } = require("../generalFunctions/dbfunctions")
 
 
 app.get("/test", async (req, res) => {
-    console.log("I'm testing");
-    res.send("Hello, World!"); // Sending a response to the client
+    console.log("working")
+
 });
+
+app.post("/registerTradesman", async (req, res) => {
+    try {
+        const { firstname, lastname, password } = req.body
+        if (firstname && lastname && password) {
+            password = hashPassword(password)
+            await registerTradesman(firstname, lastname, password)
+            res.status(200).json({ responce: "User succesfully registered" })
+        }
+        else {
+            res.status(500).json({ responce: "Not all fields were valid" })
+        }
+
+    }
+    catch (err) {
+        console.log("Error")
+    }
+})
+
+let message = hashPassword("faeem")
+console.log(message)
 
 
 
