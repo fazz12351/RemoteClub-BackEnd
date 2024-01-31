b6b8ecb3da7b5b7ee20f19819134e639fe04e7e0
Mohammed Ahmed <mahme029@gold.ac.uk>
2024-01-31 16:29:42 +0000

    Endpoint has been updated to use token instead of manually adding user.id

--- routes/tradesmanRoutes/posts-routes.js
+++ routes/tradesmanRoutes/posts-routes.js
@@ -2,31 +2,28 @@ const express = require("express");
 const app = express();
 const mongodb = require("mongodb");
 const mongoose = require("mongoose");
-const {
-    hashPassword,
-    registerEmployee,
-    EmployeeModel,
-    comparePasswords,
-    PostsModel
-} = require("../../Functions/databaseSchema");
+const { hashPassword, registerEmployee, EmployeeModel, comparePasswords, PostsModel } = require("../../Functions/databaseSchema");
+const { verifyToken } = require("../../Functions/authorisation");
 
 // This middleware is necessary to parse the request body in JSON format
 app.use(express.json());
 
 
-app.post("/post/:TradesmanId", async (req, res) => {
+
+
+//endpoint used to add new posts to the logged in user. 
+app.post("/post", verifyToken, async (req, res) => {
     try {
-        const {
-            title,
-            createdAt,
-            videoName
-        } = req.body;
-        TradesmanId = req.params.TradesmanId
+        const { title, createdAt, videoName } = req.body;
+        if (title.length < 1 || createdAt.length < 1 || videoName.length < 1) {
+            return res.status(200).json({ responce: "Ensure to add title, video and the date created" })
+        }
+        TradesmanId = req.user.id
+
         const exists = await EmployeeModel.findById(TradesmanId)
+
         if (exists) {
-            await EmployeeModel.updateOne({
-                "_id": TradesmanId
-            }, {
+            await EmployeeModel.updateOne({ "_id": TradesmanId }, {
                 $push: {
                     posts: {
                         title,
@@ -35,10 +32,8 @@ app.post("/post/:TradesmanId", async (req, res) => {
                         TradesmanId
                     }
                 }
-            }, {
-                new: true
-            }) // Return the updated document)
-
+            },
+                { new: true }) // Return the updated document)
             res.status(200).json({
                 responce: "Succesfully posted"
             })
