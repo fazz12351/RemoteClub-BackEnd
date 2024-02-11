62531cc194b0a8e4ee14f7f664168a5eafbe121c
Mohammed Ahmed <mahme029@gold.ac.uk>
2024-02-11 21:39:06 +0000

    env changes

--- Functions/databaseSchema.js
+++ Functions/databaseSchema.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 // Connect to MongoDB
-mongoose.connect("mongodb+srv://faeemahmed123:71CILCUmgi6R2uAb@remoteclub.xdttsc0.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_ATLAS_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
-
-
 
 const bcrypt = require('bcrypt');
 const saltRounds = 10; // Number of salt rounds
