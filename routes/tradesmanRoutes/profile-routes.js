9a5589ff90f491f585986922adffc60152c268c4
Mohammed Ahmed <mahme029@gold.ac.uk>
2024-05-05 01:06:10 +0100

    GET/profiel return posts and booking aswell

--- routes/tradesmanRoutes/profile-routes.js
+++ routes/tradesmanRoutes/profile-routes.js
@@ -1,19 +1,20 @@
-const express=require("express")
+const express = require("express")
 const { verifyToken } = require("../../Functions/middleware/authorisation")
 const { EmployeeModel } = require("../../Functions/databaseSchema")
-const app=express()
+const app = express()
 
 
 
-app.get("/profile",verifyToken,async(req,res)=>{
-    try{
-        const profileInfo=await EmployeeModel.findOne({_id:req.user.id})
-        const {firstname,lastname,email}=profileInfo
-        return res.status(200).json({responce:{firstname:firstname,lastname:lastname,email:email}})
+app.get("/profile", verifyToken, async (req, res) => {
+    try {
+        const profileInfo = await EmployeeModel.findOne({ _id: req.user.id })
+        const { firstname, lastname, email, posts, booking } = profileInfo
+        return res.status(200).json({ responce: { firstname: firstname, lastname: lastname, email: email, posts: posts, bookings: booking } })
     }
-    catch(err){
-        return res.status(500).json({responce:"Internal server error"})
+    catch (err) {
+        console.log(err)
+        return res.status(500).json({ responce: "Internal server error" })
     }
 })
 
-module.exports=app;
+module.exports = app;
