9bcdf38f8b5ea099a0ce14e1979f28ed8e92277b
Mohammed Ahmed <mahme029@gold.ac.uk>
2024-06-06 13:17:07 +0100

    Major updated

--- routes/tradesmanRoutes/posts-routes.js
+++ routes/tradesmanRoutes/posts-routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const { EmployeeModel,PostsModel } = require("../../Functions/databaseSchema");
+const { EmployeeModel, PostsModel } = require("../../Functions/databaseSchema");
 const multer = require("multer");
 const upload = multer();
 const { generateToken, verifyToken } = require("../../Functions/middleware/authorisation");
-const { s3Retrieve, s3Upload, s3Delete } = require("../../Functions/general_functions")
+const { s3Retrieve, s3Upload, s3Delete, getCoordinates } = require("../../Functions/general_functions")
 const { SupportApp } = require("aws-sdk");
 
 // This middleware is necessary to parse the request body in JSON format
@@ -14,15 +14,14 @@ app.use(express.json());
 // Endpoint used to add new posts to the logged-in user
 app.post("/", verifyToken, upload.any(), async (req, res) => {
     try {
-        const { title } = req.body;
+        const { title } = req.body || "";
         const date = new Date();
         const createdAt = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} @ ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
         const time = createdAt.split(" ")[0]
-        if (!title || title.length < 1 || !createdAt || createdAt.length < 1) {
-            return res.status(400).json({ response: "Ensure to add title and the date created" });
-        }
+        // if (!title || title.length < 1 || !createdAt || createdAt.length < 1) {
+        //     return res.status(400).json({ response: "Ensure to add title and the date created" });
+        // }
         const TradesmanId = req.user.id
-
 
         const exists = await EmployeeModel.findById(TradesmanId);
 
@@ -46,7 +45,7 @@ app.post("/", verifyToken, upload.any(), async (req, res) => {
                 }
             }
         });
-        const newPost=new PostsModel({
+        const newPost = new PostsModel({
             title: title,
             createdAt: createdAt,
             videoName: `${TradesmanId}@${time}${req.files[0].originalname}`,
@@ -83,27 +82,27 @@ app.get("/", verifyToken, async (req, res) => {
 })
 
 //this gets posts of other trademsmans.
-app.get("/allPosts",verifyToken,async(req,res)=>{
-    try{
+app.get("/allPosts", verifyToken, async (req, res) => {
+    try {
         //extract the id of the tradesmans
-        const user_id=req.user.id
+        const user_id = req.user.id
         //get all posts from the post table
-        const allPost=await PostsModel.find({})
+        const allPost = await PostsModel.find({})
         //filter the post by tradesmans id != current tradesman
-        const filteredPosts=[]
-        for(let i=0;i<allPost.length;i++){
-            if(allPost[i].tradesmansId!=user_id){
-                allPost[i].videoName=(allPost[i].videoName!=null?await s3Retrieve(allPost[i].videoName):null)
+        const filteredPosts = []
+        for (let i = 0; i < allPost.length; i++) {
+            if (allPost[i].tradesmansId != user_id) {
+                allPost[i].videoName = (allPost[i].videoName != null ? await s3Retrieve(allPost[i].videoName) : null)
                 filteredPosts.push(allPost[i])
             }
-            
+
         }
         //return the filtered posts
-        return res.status(200).json({data:filteredPosts})
+        return res.status(200).json({ data: filteredPosts })
     }
-    catch(err){
+    catch (err) {
         //display error if an error is caught
-        return res.status(400).json({error:errallPosts})
+        return res.status(400).json({ error: errallPosts })
     }
 })
 
