a1a7a0c6c995d32348aabdea357ab68c7e7dbd69
Mohammed Ahmed <mahme029@gold.ac.uk>
2024-09-09 02:09:04 +0100

    DynamoDB config has been set up

--- Functions/configuration.js
+++ Functions/configuration.js
@@ -4,6 +4,15 @@ const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const Twilio = require('twilio/lib/rest/Twilio');
 // Your Twilio credentials
 
+const AWS = require("aws-sdk")
+// AWS.config.update({ region: process.env.AWS_REGION })
+AWS.config.update({
+  "region": process.env.AWS_REGION,
+  "accessKeyId": process.env.AWS_ACESS_KEY,
+  "secretAccessKey": process.env.AWS_SECRET_ACESS_KEY
+});
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 
 const accountSid = process.env.ACCOUNTSID; // Replace with your Account SID from www.twilio.com/console
 const authToken = process.env.AUTHTOKENTWILIO   // Replace with your Auth Token from www.twilio.com/console
@@ -23,4 +32,8 @@ const s3Client = new S3Client({
 });
 
 
-module.exports = { s3Client, TwilioClient }
+
+
+
+
+module.exports = { s3Client, TwilioClient, dynamodb }
