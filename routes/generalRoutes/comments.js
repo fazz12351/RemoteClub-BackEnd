ecc63e8c721ae3b749c94ceded6dabc9b64a1e6a
Mohammed Ahmed <mahme029@gold.ac.uk>
2024-10-03 14:03:00 +0100

    endpoint updated so responce data stored in array format along with author firstname and surname

--- routes/generalRoutes/comments.js
+++ routes/generalRoutes/comments.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const app = express()
 const { dynamodb } = require("../../Functions/configuration")
-const { PostsModel } = require("../../Functions/databaseSchema");
+const { PostsModel, EmployeeModel, CustomerModel } = require("../../Functions/databaseSchema");
 const { verifyToken } = require("../../Functions/middleware/authorisation");
 
 
 const { v4: uuidv4 } = require('uuid'); // Import UUID library for generating unique IDs
+const { verify } = require("jsonwebtoken");
 
 //endpoint used to create a new comment for a specific post
 app.post("/:postId", verifyToken, async (req, res) => {
@@ -76,27 +77,58 @@ app.post("/:postId", verifyToken, async (req, res) => {
 });
 
 
-//endpoint to retrieve all comments for a specifc postid
+// Endpoint to retrieve all comments for a specific postId
 app.get("/:postId", verifyToken, async (req, res) => {
     try {
+        const postId = req.params.postId;
+        console.log(`Fetching comments for postId: ${postId}`);
 
-        const postId = req.params.postId
+        // DynamoDB parameters to retrieve comments for the post
         const getParams = {
             TableName: 'Comments',
-            Key: {
-                postid: postId,
-            }
+            Key: { postid: postId },
         };
+
         // Retrieve existing comments for the post
-        let data = await dynamodb.get(getParams).promise();
-        data = data.Item
-        return res.status(200).json({ responce: data.comments })
+        const data = await dynamodb.get(getParams).promise();
 
-    }
-    catch (err) {
+        // Check if there are any comments
+        if (!data.Item || !data.Item.comments) {
+            return res.status(404).json({ message: 'No comments found for this post.' });
+        }
 
+        const comments = data.Item.comments;
+        const responseData = [];
+
+        // Get the unique comment keys
+        const uniqueKeys = Object.keys(comments);
+
+        // Fetch authors and build the response data in parallel
+        await Promise.all(uniqueKeys.map(async (key) => {
+            const comment = comments[key];
+            // Try to find the author in either EmployeeModel or CustomerModel
+            const author = await EmployeeModel.findOne({ _id: comment.userId }) ||
+                await CustomerModel.findOne({ _id: comment.userId });
+
+            // Add author's details to the comment
+            if (author) {
+                comment.firstname = author.firstname;
+                comment.surname = author.surname;
+            } else {
+                comment.firstname = "Unknown";
+                comment.surname = "User";
+            }
+            responseData.push(comment);
+        }));
+
+        // Return the comments with author details
+        return res.status(200).json({ comments: responseData });
+    } catch (err) {
+        console.error('Error fetching comments:', err);
+        return res.status(500).json({ message: 'An error occurred while retrieving comments.' });
     }
-})
+});
+
 
 
 
